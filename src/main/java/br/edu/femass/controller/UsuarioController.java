8904c95fcad13a5f5e3ea2b20028dc1ea2449bd0
DeyvidManhaes <deyvidmanhaes2@gmail.com>
2023-06-26 19:48:02 -0300

    alteracoes

--- src/main/java/br/edu/femass/controller/UsuarioController.java
+++ src/main/java/br/edu/femass/controller/UsuarioController.java
@@ -7,10 +7,8 @@ import br.edu.femass.dao.DaoGenero;
 import br.edu.femass.dao.DaoLivro;
 import br.edu.femass.entities.Genero;
 import br.edu.femass.entities.Livro;
-import br.edu.femass.utils.DiversosJavaFx;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -50,92 +48,36 @@ public class UsuarioController implements Initializable {
         if (livro==null) return;
 
 
-        TxtAutor.setText(livro.getAutor());
-        TxtCopias.
+        TxtAutor.setText(livro.getAutor().toString());
+        
         CboGenero.getSelectionModel().select(livro.getGenero());
     }
 
-    @FXML
-    private void BtnExcluir_Click(ActionEvent event) {
-        Medico medico = listaMedico.getSelectionModel().getSelectedItem();
-        if (medico ==null) return;
+    
 
+
+    
+
+    public void exibirlivros() {
         try {
-            if (medicoDao.excluir(medico)==false) {
-                DiversosJavaFx.exibirMensagem("Não foi possível excluir o cliente selecionado");
-            }
-        exibirMedicos();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       
-
-    }
-
-    @FXML
-    private void BtnGravar_Click(ActionEvent event) {
-        try {
-            Medico medico = new Medico(
-                    TxtNome.getText(),
-                    TxtCRM.getText(),                    
-                    TxtCpf.getText(),
-                    TxtTelefone.getText(),
-                    CboEspecialidade.getSelectionModel().getSelectedItem()
-                    );
-            medico.setEmail(TxtEmail.getText());
-            medico.setEndereco(TxtEndereco.getText());
-
-
-            if (medicoDao.gravar(medico)==false) {
-                DiversosJavaFx.exibirMensagem("Não foi possível gravar o cliente");
-                return;
-            }
-            TxtNome.setText("");
-            TxtCpf.setText("");
-            TxtTelefone.setText("");
-            TxtEmail.setText("");
-            TxtEndereco.setText("");
-            TxtCRM.setText("");
-            CboEspecialidade.getSelectionModel().select(null);
+        ObservableList<Livro> data = FXCollections.observableArrayList(
             
-            
-
-
-            exibirMedicos();   
-        } catch (Exception e) {
-            DiversosJavaFx.exibirMensagem(e.getMessage());
-        }
-
-
-    }
-
-    public void exibirMedicos() {
-        try {
-        ObservableList<Medico> data = FXCollections.observableArrayList(
-            medicoDao.buscarAtivos()
         );
-        listaMedico.setItems(data);
+        listalivros.setItems(data);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         
     }
-    public void exibirEspecialidade(){
-        try{
-            ObservableList<Especialidade> data = FXCollections.observableArrayList(
-                especialidadeDao.buscarAtivos()
-            );
-            CboEspecialidade.setItems(data);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }       
-    }
+    
 
  @Override
 public void initialize(URL location, ResourceBundle resource){
-    exibirMedicos();
-    exibirEspecialidade();
+    exibirlivros();
+    
    }
 
 }
+
+
 
