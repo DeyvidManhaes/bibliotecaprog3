3ec99a4a7c8f6ebca6f76f62a361047ac711c045
DeyvidManhaes <deyvidmanhaes2@gmail.com>
2023-06-27 23:31:33 -0300

    Inicio implementação tela cadastro usuário:
    Implementado ChoiceBox Tipo de usuário.
    Text Fildes para cadastro

--- src/main/java/br/edu/femass/controller/CadastroUsuarioController.java
+++ src/main/java/br/edu/femass/controller/CadastroUsuarioController.java
@@ -1,5 +1,51 @@
 package br.edu.femass.controller;
 
-public class CadastroUsuarioController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.application.Application;
+
+public class CadastroUsuarioController implements Initializable{
+
+    @FXML
+    private ChoiceBox<String> CBoxTipo = new ChoiceBox<>();
+    @FXML
+    private TextField TxtNome;
+    @FXML
+    private TextField TxtTelefone;
+    @FXML
+    private TextField TxtEmail;
+    @FXML
+    private TextField TxtLogin;
+    @FXML
+    private TextField TxtSenha;
+    @FXML
+    private TextField TxtMatricula;
+    @FXML
+    private TextField TxtFormação;
+
+    @FXML
+    
+    
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        CBoxTipo.getItems().addAll("Professor","Aluno");
+
+     
+
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+
     
 }
